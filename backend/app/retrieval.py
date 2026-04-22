334fbdf67e08d3260dfd761e838b7edd45a2acc3
Pabodha Wanniarachchi <pabodhawanniarachchi@gmail.com>
2026-04-22 22:43:32 +0530

    feat: implement semantic search retrieval endpoint

--- backend/app/retrieval.py
+++ backend/app/retrieval.py
@@ -1,3 +1,43 @@
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.vectorstores import Chroma
+import os
+
+DB_DIR = os.path.join(os.getcwd(),"chroma_store")
+
+def search_codebase(query:str,k : int=5):
+    print(f"Searching database for: `{query}`")
+
+
+    
+    embedding_model = HuggingFaceEmbeddings(
+        model_name="all-MiniLM-L6-v2"
+    )
+
+    # load the Chroma store
+    vectorstore = Chroma(
+        persist_directory=DB_DIR,
+        embedding_function=embedding_model
+    )
+
+    # Perform a mathemetical similarity
+    results = vectorstore.similarity_search(query,k=k)
+
+    formatted_results = []
+
+    for doc in results:
+        formatted_results.append({
+            "source":doc.metadata.get("source","Unknown File"),
+            "content":doc.page_content
+        })
+
+
+    print(f"Found {len(formatted_results)} matching chunks!")
+    return formatted_results
+
+
+
+
+
 
 
 
--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
 from app.ingestion import clone_and_embed
+from app.retrieval import search_codebase
 
 router = APIRouter()
 
@@ -8,6 +9,8 @@ router = APIRouter()
 class RepoRequest(BaseModel):
     url:str
 
+class ChatRequest(BaseModel):
+    question:str
 
 @router.post("/ingest-repo")
 async def ingest_repo(request:RepoRequest):
@@ -21,6 +24,23 @@ async def ingest_repo(request:RepoRequest):
 
         }
 
+
+    except Exception as e:
+        raise HTTPException(status_code=400,detail=str(e))
+    
+
+@router.post("/chat")
+async def chat_with_repo(request: ChatRequest):
+    try:
+        results = search_codebase(request.question)
+
+        if not results:
+            return {"answer":"I couldn't find any code related to that question.","sources": []}
+        
+        return{
+            
+            "sources":results
+        }
 
     except Exception as e:
         raise HTTPException(status_code=400,detail=str(e))
