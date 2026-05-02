8114141fcb156e12223688dbe1b15261f187de9c
Pabodha Wanniarachchi <pabodhawanniarachchi@gmail.com>
2026-05-03 00:20:26 +0530

    build a secure api bridge

---
+++ frontend/src/app/api/chat/routes.ts
@@ -0,0 +1,22 @@
+import { NextResponse } from "next/server";
+
+export async function POST(req:Request){
+    try{
+        const { messages } = await req.json();
+
+        const pythonResponse =  await fetch('http://127.0.0.1:8000/chat',{
+            method:"POST",
+            headers:{'Content-Type':"application/json"},
+            body:JSON.stringify({messages})
+        })
+
+        const data = await pythonResponse.json()
+        return NextResponse.json(data)
+
+    }catch(error){
+        return NextResponse.json(
+            {error:"Failed to connect python backend"},
+            {status:500}
+        )
+    }
+}
