39cff684fe0cd1f283c85a42cec06dae318e84f8
Pabodha Wanniarachchi <pabodhawanniarachchi@gmail.com>
2026-05-26 02:17:55 +0530

    Refactor db.py to use a VectorDB class

--- backend/app/db.py
+++ backend/app/db.py
@@ -2,19 +2,67 @@ import os
 from langchain_postgres.vectorstores import PGVector
 from dotenv import load_dotenv
 from langchain_huggingface import HuggingFaceEndpointEmbeddings
+import logging
+from langchain_core.documents import Document
+from typing import List
+from exceptions import DatabaseError
+from config import DATABASE_URL, HUGGINGFACE_TOKEN, EMBEDDING_MODEL
 
-load_dotenv()
+logger = logging.getLogger(__name__)
 
-HF_TOKEN = os.getenv("HUGGINGFACEHUB_API_TOKEN")
-CONNECTION_STRING = os.getenv("DATABASE_URL")
+class VectorDB:
+    def __init__(self):
+        try:
+            self.embeddings = HuggingFaceEndpointEmbeddings(
+                model=EMBEDDING_MODEL,
+                huggingfacehub_api_token=HUGGINGFACE_TOKEN
+            )
 
-embeddings = HuggingFaceEndpointEmbeddings(
-    model="sentence-transformers/all-MiniLM-L6-v2",
-    huggingfacehub_api_token=HF_TOKEN
-)
+            self.client = PGVector(
+                embeddings=self.embeddings,
+                collection_name="code_collection",
+                connection=DATABASE_URL
+            )
 
-db = PGVector(
-    embeddings=embeddings,
-    collection_name="code_collection",
-    connection=CONNECTION_STRING
-)
+            logger.info("Vector DB initialized")
+
+        except Exception as e:
+            logger.error(f"Vector DB init failed: {str(e)}")
+            raise DatabaseError(f"Database initialization failed: {str(e)}")
+    
+
+
+    """Add documents to vector DB"""
+    def add_document(self,documents:List[Document])->None:
+        try:
+            self.client.add_documents(documents)
+            logger.info(f"Added {len(documents)} documents")
+        except Exception as e:
+            logger.error(f"Failed to add documents: {str(e)}")
+            raise DatabaseError(f"Failed to store documents:{str(e)}")
+    
+
+    """Search for similar documents"""
+    def similarity_search(self,query:str,k:int=5) -> List[Document]:
+        try:
+            logger.debug("Searching:{query}")
+            results = self.client.similarity_search(query,k=k)
+            logger.info(f"Found {len(results)} results")
+            return results
+        except Exception as e:
+            logger.error(f"Search failed: {str(e)}")
+            raise DatabaseError(f"Vector search failed: {str(e)}")
+
+
+    """Delete collection for re-ingestion"""
+    def delete_collection(self) -> None:
+        try:
+            self.client.delete_collection()
+            logger.info("Collection deleted")
+        except Exception as e:
+            logger.error(f"Failed to delete collection: {str(e)}")
+            raise DatabaseError(f"Failed to clear collection: {str(e)}")
+
+
+db = VectorDB()
+
